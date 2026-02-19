30fb7e8d6473b11fcda6ef73ac28e62dfe701010
sivasai-01 <chatrasisivasai@gmail.com>
2026-02-19 12:43:28 +0530

    modified api base in livekitservice

--- frontend/src/services/livekitService.js
+++ frontend/src/services/livekitService.js
@@ -1,6 +1,6 @@
 import { Room, createLocalAudioTrack } from 'livekit-client';
 
-const API_BASE = "http://localhost:8000";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:8000";
 
 class LiveKitService {
   constructor() {
